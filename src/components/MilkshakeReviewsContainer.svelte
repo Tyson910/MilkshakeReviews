6f4fa8331979a0cdaeab1cd90d3d1c24332bab6c
Tyson <TysonS.417@gmail.com>
2023-06-03 22:27:41 -0400

    create milkshakereviewscontainer component

---
+++ src/components/MilkshakeReviewsContainer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import MilkshakeReview from '@components/MilkshakeReview.svelte';
+  import { searchResults } from '@utils/stores';
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto mt-8">
+  {#each $searchResults as { explanation, dishName, stars }}
+    <MilkshakeReview {explanation} {dishName} {stars} />
+  {/each}
+</div>
