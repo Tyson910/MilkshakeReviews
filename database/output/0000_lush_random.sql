bc832cb2451e9534d3baee5befc54f5ba93ae5e5
Tyson <TysonS.417@gmail.com>
2024-01-30 22:31:35 -0700

    set up init schema

---
+++ database/output/0000_lush_random.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `eateries` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`reveiw` text NOT NULL,
+	`metro` text NOT NULL,
+	`address` text,
+	`phone` text,
+	`websiteURL` text
+);
